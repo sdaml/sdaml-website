75b92161d6d338c15b739dca2685711fd0ee12d3
Jake Runzer <jakerunzer@gmail.com>
2017-02-15 16:12:26 -0800

    fix meet dates and locations

--- src/js/data/meets.js
+++ src/js/data/meets.js
@@ -12,49 +12,49 @@ const meets = [
     }, {
         title: '🤖 Slack bot',
         author: 'Jake',
-        location: 'MAC 123',
-        dateStart: moment('2017-02-08 18:30:00'),
-        dateEnd: moment('2017-02-08 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-02-22 18:30:00'),
+        dateEnd: moment('2017-02-22 20:00:00'),
         githubLink: null,
         description: 'The first thing we will be building together is a basic Slack bot, using NodeJS, ES6, and the Slack APIs. We will also learn how to host it on Heroku.'
     }, {
         title: '🎤 NLP Basics',
         author: 'Jair',
-        location: 'MAC 123',
-        dateStart: moment('2017-02-15 18:30:00'),
-        dateEnd: moment('2017-02-15 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-03-01 18:30:00'),
+        dateEnd: moment('2017-03-01 20:00:00'),
         githubLink: null,
         description: 'This week we are taking our first steps into machine learning, starting with natural language processing!'
     }, {
         title: '⚡️ NLP + Slack',
         author: 'Jake',
-        location: 'MAC 123',
-        dateStart: moment('2017-02-22 18:30:00'),
-        dateEnd: moment('2017-02-22 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-03-08 18:30:00'),
+        dateEnd: moment('2017-03-08 20:00:00'),
         githubLink: null,
         description: 'Using our NLP knowledge from last week, we will add natural language processing to our Slack bot to make it even more awesome.'
     }, {
         title: '🤔 Functional Paradigms',
         author: 'Jake',
-        location: 'MAC 123',
-        dateStart: moment('2017-03-01 18:30:00'),
-        dateEnd: moment('2017-03-01 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-03-15 18:30:00'),
+        dateEnd: moment('2017-03-15 20:00:00'),
         githubLink: null,
         description: 'What is functional programming and how does it compare to OOP? These are the questions that will be answered this week. We will be using Javascript and Python to apply functional paradigms to common problems.'
     }, {
         title: '✨ Kickstart your Portfolio',
         author: 'Team',
-        location: 'MAC 123',
-        dateStart: moment('2017-03-08 18:30:00'),
-        dateEnd: moment('2017-03-08 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-03-22 18:30:00'),
+        dateEnd: moment('2017-03-22 20:00:00'),
         githubLink: null,
         description: 'Project week where you can work on your portfolio or a side project with the sd&ml members around to help.'
     }, {
         title: '🚀 Deployment Checklist',
         author: 'Tyson',
-        location: 'MAC 123',
-        dateStart: moment('2017-03-15 18:30:00'),
-        dateEnd: moment('2017-03-15 20:00:00'),
+        location: 'SUB Upper Lounge',
+        dateStart: moment('2017-03-29 18:30:00'),
+        dateEnd: moment('2017-03-29 20:00:00'),
         githubLink: null,
         description: 'Learn what it takes to deploy your project to production. What is git flow, CI, Nginx, SSL.'
     }
