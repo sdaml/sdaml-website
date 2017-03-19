25352604f92f53bac838757180aab9b4db613f89
Jake Runzer <jakerunzer@gmail.com>
2017-03-18 18:20:20 -0700

    reorg meets

--- src/js/data/meets.js
+++ src/js/data/meets.js
@@ -26,46 +26,47 @@ const meets = [
         githubLink: null,
         description: 'Come work on a side project in the company of the sd&ml team.'
     }, {
-        title: '🎤 NLP Basics',
-        author: 'Jair',
-        location: 'SUB Upper Lounge',
-        dateStart: moment('2017-03-08 18:30:00'),
-        dateEnd: moment('2017-03-08 20:00:00'),
-        githubLink: null,
-        description: 'This week we are taking our first steps into machine learning, starting with natural language processing!'
-    }, {
-        title: '⚡️ NLP + Slack',
-        author: 'Jake',
-        location: 'SUB Upper Lounge',
+        title: '🤔 AI Gameplaying',
+        author: 'Zev',
+        location: 'MAC 123',
         dateStart: moment('2017-03-15 18:30:00'),
         dateEnd: moment('2017-03-15 20:00:00'),
-        githubLink: null,
-        description: 'Using our NLP knowledge from last week, we will add natural language processing to our Slack bot to make it even more awesome.'
-    }, {
-        title: '🤔 Functional Paradigms',
-        author: 'Jake',
-        location: 'SUB Upper Lounge',
-        dateStart: moment('2017-03-22 18:30:00'),
-        dateEnd: moment('2017-03-22 20:00:00'),
-        githubLink: null,
-        description: 'What is functional programming and how does it compare to OOP? These are the questions that will be answered this week. We will be using Javascript and Python to apply functional paradigms to common problems.'
-    }, {
-        title: '✨ Kickstart your Portfolio',
-        author: 'Team',
-        location: 'SUB Upper Lounge',
-        dateStart: moment('2017-03-29 18:30:00'),
-        dateEnd: moment('2017-03-29 20:00:00'),
-        githubLink: null,
-        description: 'Project week where you can work on your portfolio or a side project with the sd&ml members around to help.'
+        githubLink: 'https://github.com/sdaml/ai-gameplaying',
+        description: 'Zev will be taking us through some AI techniques for 2 player optimization games.'
     }, {
         title: '🚀 Deployment Checklist',
         author: 'Tyson',
         location: 'SUB Upper Lounge',
-        dateStart: moment('2017-04-05 18:30:00'),
-        dateEnd: moment('2017-04-05 20:00:00'),
+        dateStart: moment('2017-04-29 18:30:00'),
+        dateEnd: moment('2017-04-29 20:00:00'),
         githubLink: null,
         description: 'Learn what it takes to deploy your project to production. What is git flow, CI, Nginx, SSL.'
     }
+    // {
+    //     title: '⚡️ NLP + Slack',
+    //     author: 'Jake',
+    //     location: 'SUB Upper Lounge',
+    //     dateStart: moment('2017-03-15 18:30:00'),
+    //     dateEnd: moment('2017-03-15 20:00:00'),
+    //     githubLink: null,
+    //     description: 'Using our NLP knowledge from last week, we will add natural language processing to our Slack bot to make it even more awesome.'
+    // }, {
+    //     title: '🤔 Functional Paradigms',
+    //     author: 'Jake',
+    //     location: 'SUB Upper Lounge',
+    //     dateStart: moment('2017-03-22 18:30:00'),
+    //     dateEnd: moment('2017-03-22 20:00:00'),
+    //     githubLink: null,
+    //     description: 'What is functional programming and how does it compare to OOP? These are the questions that will be answered this week. We will be using Javascript and Python to apply functional paradigms to common problems.'
+    // }, {
+    //     title: '✨ Kickstart your Portfolio',
+    //     author: 'Team',
+    //     location: 'SUB Upper Lounge',
+    //     dateStart: moment('2017-03-29 18:30:00'),
+    //     dateEnd: moment('2017-03-29 20:00:00'),
+    //     githubLink: null,
+    //     description: 'Project week where you can work on your portfolio or a side project with the sd&ml members around to help.'
+    // }
 ];
 
 export default meets;
