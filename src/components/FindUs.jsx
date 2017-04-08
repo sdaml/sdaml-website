81dee3ad305724c1beeab15121f17350aac784e9
Jake Runzer <jakerunzer@gmail.com>
2017-04-08 01:37:29 -0700

    add slack link

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -4,24 +4,24 @@ import Section from './Section.jsx';
 
 // Logos
 import Facebook from '../assets/facebook_logo.svg';
-// import Slack from '../assets/slack_logo.svg';
+import Slack from '../assets/slack_logo.svg';
 import Twitter from '../assets/twitter_logo.svg';
-
-// {
-//     name: 'Slack',
-//     icon: Slack,
-//     href: '#'
-// }
 
 const findUsLinks = [
     {
         name: 'Facebook',
         icon: Facebook,
         href: 'https://www.facebook.com/groups/uvic.software'
-    }, {
+    },
+    {
         name: 'Twitter',
         icon: Twitter,
         href: 'https://twitter.com/uvicsdaml'
+    },
+    {
+        name: 'Slack',
+        icon: Slack,
+        href: 'https://sdaml.slack.com'
     }
 ];
 
@@ -32,18 +32,27 @@ class FindUs extends Component {
 
     render() {
         return (
-            <Section {...{
-                title: 'Find Us',
-                noGrid: true
-            }}>
+            <Section
+                {...{
+                    title: 'Find Us',
+                    noGrid: true
+                }}
+            >
 
                 {findUsLinks.map(function(findLink) {
-                    return <div key={findLink.name} className="find-link">
-                        <a href={findLink.href}>
-                            <img className="find-icon vert-align-middle" srcSet={findLink.icon} />
-                            <span className="find-name bold">{findLink.name}</span>
-                        </a>
-                    </div>;
+                    return (
+                        <div key={findLink.name} className="find-link">
+                            <a href={findLink.href}>
+                                <img
+                                    className="find-icon vert-align-middle"
+                                    srcSet={findLink.icon}
+                                />
+                                <span className="find-name bold">
+                                    {findLink.name}
+                                </span>
+                            </a>
+                        </div>
+                    );
                 })}
 
             </Section>
